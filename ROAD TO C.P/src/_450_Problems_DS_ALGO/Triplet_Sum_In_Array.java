61922f89e99247ddaf0e7f3469adba6ef09a8be3
saikat1 <saikat1@saikat>
2021-02-06 03:58:16 +0530

    Commit

--- ROAD TO C.P/src/_450_Problems_DS_ALGO/Triplet_Sum_In_Array.java
+++ ROAD TO C.P/src/_450_Problems_DS_ALGO/Triplet_Sum_In_Array.java
@@ -1,10 +1,50 @@
 package _450_Problems_DS_ALGO;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Triplet_Sum_In_Array {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int k=s.nextInt();
+		int arr[]=new int[n];
+		for (int i = 0; i < arr.length; i++) 
+		{
+			arr[i]=s.nextInt();
+		}
+		boolean ans=func(arr,n,k);
+		System.out.println(ans);
 
+	}
+
+	private static boolean func(int[] arr, int n, int k) 
+	{
+		
+		Arrays.sort(arr);
+		for(int i=0;i<arr.length-2;i++)
+		{
+			int start=i+1;
+			int end=n-1;
+			while(start<end)
+			{
+				if(arr[i]+arr[start]+arr[end]==k)
+				{
+					return true;
+				}
+				else if(arr[i]+arr[start]+arr[end]<k)
+				{
+					start++;
+				}
+				else
+				{
+					end--;
+				}
+			}
+		}
+		return false;
 	}
 
 }
