2ae03d928f20904a47ee0424cb48eecd26a11b12
Saikat-Chowdhury90 <sonusaikat09@gmail.com>
2021-04-11 16:44:40 +0530

    comiit

--- ROAD TO C.P/src/_450_Problems_DS_ALGO/String_Rotation_of_each_other_or_not.java
+++ ROAD TO C.P/src/_450_Problems_DS_ALGO/String_Rotation_of_each_other_or_not.java
@@ -13,7 +13,7 @@ public class String_Rotation_of_each_other_or_not {
 			int j=0;
 			for(int i=(temp.length()/2)-1;i<temp.length();i++,j++)
 			{
-				System.out.print(temp.substring(j, i+1) +" ");
+				
 				if(str2.equals(temp.substring(j, i+1)))
 				{
 					
